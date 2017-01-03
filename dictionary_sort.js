a042f3bd71bdb08a9300612b5796aa0d85604493
Syanmil <syanmil@yahoo.com>
2017-01-03 10:10:33 +0700

    initialize readline for good

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -5,3 +5,15 @@ class Dictionary {
 
 const readline = require('readline');
 // your code here to initialize the program and take user input
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+var myWord = []
+rl.on('line', (words) => {
+  if (words != "/n"){
+    myWord.push(words)
+  } else {
+    rl.close
+  }
+})

f77ad951c55c018e09ba31c58579c18d6c04783b
Irsan Sebastian <email": null>
2017-01-03 20:30:35 +0700

    First Commit

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,7 +1,29 @@
-class Dictionary {
-  // Your code here to receive user input & sort the array
-
-}
-
 const readline = require('readline');
 // your code here to initialize the program and take user input
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.setPrompt("Type a word : ");
+rl.prompt();
+  var arr = [];
+
+rl.on("line", (answer) => {
+
+
+
+      if(answer === "")
+      {
+        rl.close();
+        console.log("Congratulation your dictionary has "+arr.length+" word");
+        console.log(arr);
+        // return console.log(arr.sort().join("-"));;
+      }else{
+
+        rl.setPrompt("Type another word (Or PRESS ENTER TO FINISH) : ");
+        rl.prompt();
+        arr.push(answer);
+      }
+});

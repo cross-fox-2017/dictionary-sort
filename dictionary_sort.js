ce3afa85a53f51a81e08d00f3ee402de3e236b3f
Syanmil <syanmil@yahoo.com>
2017-01-03 14:08:21 +0700

    sorting using insertion done

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -3,17 +3,36 @@ class Dictionary {
 
 }
 
-const readline = require('readline');
-// your code here to initialize the program and take user input
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-var myWord = []
-rl.on('line', (words) => {
-  if (words != "/n"){
-    myWord.push(words)
-  } else {
-    rl.close
+// const readline = require('readline');
+// // your code here to initialize the program and take user input
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+// var myWord = []
+// rl.on('line', (words) => {
+//   if (words == ""){
+//     rl.close();
+//     console.log(myWord);
+//     sorting(myWord);
+//   } else {
+//     myWord.push(words);
+//   }
+// })
+
+function sorting(myWord){
+  var temp = ""
+  for (var i = 0; i < myWord.length; i++) {
+    for (var j = 0; j < myWord[i].length; j++) {
+      if (myWord[i] < myWord[j]){
+        temp = myWord[j]
+        myWord[j] = myWord[i]
+        myWord[i] = temp
+      }
+    }
+    // myWord[i] = myWord[i].split("");
   }
-})
+  return myWord
+}
+
+console.log(sorting(["siapa", "dia", "aku", "kamu", "kita"]));

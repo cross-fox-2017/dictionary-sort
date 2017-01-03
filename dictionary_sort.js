a4e5fa5c9b3c6e09d18da6b3a835c34391a03630
Dale <dellryuzi@gmail.com>
2017-01-03 22:53:27 +0700

    done

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,7 +1,21 @@
-class Dictionary {
-  // Your code here to receive user input & sort the array
-
-}
 
 const readline = require('readline');
+const rl = readline.createInterface({
+  //input: fs.createReadStream('sample.txt')
+  input:process.stdin,
+  output:process.stdout
+});
 // your code here to initialize the program and take user input
+var tampung = [];
+rl.on('line', (line) => {
+
+  if(line === ""){
+    tampung.sort(function (a, b) {
+    return a.toLowerCase()<b.toLowerCase()?-1:(a.toLowerCase()<b.toLowerCase()?1:0);
+});
+  console.log(tampung.join(" "));
+  //console.log(tampung);
+  rl.close();
+  }
+  tampung.push(line);
+});

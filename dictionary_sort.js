8ae51b42aa42111e4f564cd5055c70779186ddd9
Irsan Sebastian <email": null>
2017-01-04 11:01:11 +0700

    Final Commit

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -8,37 +8,53 @@ const rl = readline.createInterface({
 
 rl.setPrompt("Type a word : ");
 rl.prompt();
-  var arr = [];
+
   var key = [];
   var result =[];
+
 rl.on("line", (answer) => {
-
-
 
       if(answer === "")
       {
-        rl.close();
-        console.log("Congratulation your dictionary has "+arr.length+" word");
-        console.log(arr);
 
-        for(var count = 0; count < arr.length;count++)
-        {ßßß
-          if(arr[count].toUpperCase())
-          {
-              key.push(count);
-          }else{
-            continue;
-          }
+        rl.close();
+
+        console.log("Congratulation your dictionary has "+key.length+" word");
+        for(var x = 0; x < key.length;x++){
+
+
+              if(x === key.length-1)
+              {
+                console.log('if x === key');
+                result.push(key[x]);
+              }else{
+                console.log('compare');
+
+                if(key[x].toLowerCase() > key[x+1].toLowerCase())
+                {
+                  var temp = key[x];
+                  key[x] = key[x+1];
+                  key[x+1] = temp;
+                }
+              }
+
+
+
         }
-        for(var i = 0; i < arr.length; i++)
-        {
-          if(arr[i] == )
-        }
+          console.log("-"+key);
+
+
+        // arr.sort(function(a,b){
+        //
+        //       return a.toLowerCase().localeCompare(b.toLowerCase);
+        // })
+        //
+        //   return console.log(arr.join("-"));
         // return console.log(arr.sort().join("-"));;
       }else{
 
         rl.setPrompt("Type another word (Or PRESS ENTER TO FINISH) : ");
         rl.prompt();
-        arr.push(answer);
+        key.push(answer);
       }
 });

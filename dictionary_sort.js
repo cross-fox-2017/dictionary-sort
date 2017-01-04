57de3af3799d818de5de5785f2cadecf677dfd1a
timocodex <timothy.og.siahaan@gmail.com>
2017-01-04 18:30:02 +0700

    final dictionary

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,7 +1,42 @@
-class Dictionary {
-  // Your code here to receive user input & sort the array
+var arr=[];
 
+function dictionary(array){
+  array.sort(function (a,b){
+    return a.toLowerCase().localeCompare(b.toLowerCase());
+  });
+}
+function susun(arra){
+  var list=''
+  for(var i=0;i< arra.length;i++){
+    list += arra[i]+ '\n';
+  }
+  return list;
 }
 
 const readline = require('readline');
-// your code here to initialize the program and take user input
+const rl = readline.createInterface({
+
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'Type a word:  '
+});
+
+rl.prompt();
+
+rl.on('line', (line) => {
+
+
+  if(line===''){
+    rl.close();
+
+  }
+  else{
+  arr.push(line);
+  }
+rl.prompt();
+
+}).on('close', () => {
+  dictionary(arr);
+  console.log(`congratulations! your dictionary has  ${arr.length} words \n${susun(arr)}`);
+  process.exit(0);
+});

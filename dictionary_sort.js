d75e3d698be07ece9869a86fd1116bc49ee47b4f
pisanggoreng <7ofpentacles@gmail.com>
2017-01-05 14:59:47 +0700

    clear release 0

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,7 +1,26 @@
-class Dictionary {
-  // Your code here to receive user input & sort the array
-
-}
-
 const readline = require('readline');
 // your code here to initialize the program and take user input
+
+const rl = readline.createInterface({
+ input: process.stdin,
+ output: process.stdout
+});
+
+rl.setPrompt("Type a word :");
+rl.prompt();
+
+var tampung = [];
+var sort = []
+
+rl.on('line', (input) => {
+  if (input == "") {
+    rl.close();
+    console.log(tampung);
+
+    console.log( "Congratulation your dictionary has " + tampung.length + " word");
+  } else {
+    rl.setPrompt("Type another word ( Or press enter to finish) :");
+    rl.prompt();
+    tampung.push(input);
+  }
+});

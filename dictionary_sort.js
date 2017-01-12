2532f06ebcb509340f4b6b99d60e7408f25f1f67
Wahyuhidayatt <wahyuhdyt95@gmail.com>
2017-01-12 08:55:54 +0700

    dictionary

--- dictionary_sort.js
+++ dictionary_sort.js
@@ -1,7 +1,32 @@
 class Dictionary {
-  // Your code here to receive user input & sort the array
-
 }
-
+  // Your code here to receive user input & sort the array
 const readline = require('readline');
 // your code here to initialize the program and take user input
+const rl = readline.createInterface({
+  input : process.stdin,
+  output : process.stdot,
+});
+
+var arr = [];
+console.log("Input kata : ");
+
+rl.on('line', (kata) => {
+	var arrWord = arr.sort(function(a,b){return a.toLowerCase().localeCompare(b.toLowerCase())});
+	
+  if(kata == ""){
+    console.log("Congratulation! " + arr.length + " words");
+    for(var i=0; i < arrWord.length; i++){
+		console.log(arrWord[i]);
+    }
+	
+	console.log(arrWord);
+    rl.close()
+  }
+  else{
+    arr.push(kata);
+    console.log("Enter untuk mengakhiri ");
+  		}
+
+  	});
+
